0b01953830c8e8e861cf7bf4958e2842596612e0
OZahed <omidzahed8@gmail.com>
2024-03-08 18:43:15 +0330

    feat: added sql connection probing

---
+++ db/msql/mysql.go
@@ -0,0 +1,36 @@
+package msql
+
+import (
+	//nolint:revive
+	"fmt"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type MySQLConnStringProvider struct {
+	Host         string
+	Port         int
+	Proto        string
+	User         string
+	Password     string
+	DatabaseName string
+}
+
+func (s *MySQLConnStringProvider) Name() string {
+	return "mysql"
+}
+
+func (s *MySQLConnStringProvider) ConnectionString() string {
+	if strings.TrimSpace(s.Proto) == "" {
+		s.Proto = "tcp"
+	}
+
+	return fmt.Sprintf("%s:%s@%s(%s:%d)/%s",
+		s.User, s.Password, s.Proto, s.Host, s.Port, s.DatabaseName,
+	)
+}
+
+func (s *MySQLConnStringProvider) DBName() string {
+	return s.DatabaseName
+}
---
+++ db/psql/postgres.go
@@ -0,0 +1,41 @@
+package psql
+
+import (
+	//nolint:revive
+
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type PostgreSQLConnectionStringProvider struct {
+	Host         string
+	Port         int
+	User         string
+	Password     string
+	DatabaseName string
+	SSL          bool
+}
+
+func (s *PostgreSQLConnectionStringProvider) Name() string {
+	return "postgres"
+}
+
+func (s *PostgreSQLConnectionStringProvider) DBName() string {
+	return s.DatabaseName
+}
+
+func (s *PostgreSQLConnectionStringProvider) ConnectionString() string {
+	sslMode := "disable"
+	if s.SSL {
+		sslMode = "enable"
+	}
+
+	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		s.User, s.Password, s.Host, s.Port, s.DatabaseName, sslMode,
+	)
+}
+
+func NewPostgreSQLDriverConn() *PostgreSQLConnectionStringProvider {
+	panic("not implemented")
+}
