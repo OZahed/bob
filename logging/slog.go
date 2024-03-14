3040fe3f169d61b8d258ff5bf0d70716463e0b35
ferizoozoo <farhadzohoorz@gmail.com>
2024-03-14 23:47:57 +0330

    Remove singleton initialization of slog logger and and WithUTCTime function for replaceAttr

--- logging/slog.go
+++ logging/slog.go
@@ -1,12 +1,10 @@
 package logging
 
 import (
-	"errors"
 	"log/slog"
 	"os"
+	"time"
 )
-
-var logger *slog.Logger
 
 // HandlerType determines which type of Handler should be used for the logger
 type HandlerType int
@@ -16,25 +14,39 @@ const (
 	Text
 )
 
-// Initiate function provides a new logger instance from the slog package
+// NewSlog function provides a new logger instance from the slog package
 // with the provided options.
-func Initiate(handler HandlerType, level slog.Level) {
+func NewSlog(handler HandlerType, level slog.Level, name string) *slog.Logger {
+	var handlerFunc slog.Handler
+
 	handlerOptions := &slog.HandlerOptions{
-		AddSource: true,
-		Level:     level,
+		AddSource:   true,
+		Level:       level,
+		ReplaceAttr: WithUTCTime,
 	}
-	if handler == JSON {
-		logger = slog.New(slog.NewJSONHandler(os.Stdout, handlerOptions))
-	} else if handler == Text {
-		logger = slog.New(slog.NewTextHandler(os.Stdout, handlerOptions))
+	switch handler {
+	case JSON:
+		handlerFunc = slog.NewJSONHandler(os.Stdout, handlerOptions)
+	default:
+		handlerFunc = slog.NewTextHandler(os.Stdout, handlerOptions)
 	}
+
+	if name != "" {
+		handlerFunc = handlerFunc.WithAttrs([]slog.Attr{slog.String("name", name)})
+	}
+
+	return slog.New(handlerFunc)
 }
 
-// Get function returns the logger instance once initialized or returns error, when
-// a logger instance could not be created.
-func Get() (*slog.Logger, error) {
-	if logger == nil {
-		return nil, errors.New("logger is not initiated")
+// WithUTCTime replaces the time attr value from Unix timestamps to UTC date
+// and duration to a string representation of it.
+func WithUTCTime(groups []string, a slog.Attr) slog.Attr {
+	switch a.Key {
+	case slog.TimeKey:
+		a.Value = slog.TimeValue(time.Now().UTC())
+	case slog.KindDuration.String():
+		a.Value = slog.StringValue(a.Value.Duration().String())
 	}
-	return logger, nil
+
+	return a
 }
