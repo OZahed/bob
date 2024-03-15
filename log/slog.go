d1c2a1664a4bb3acdafd5b418121e213e7f80429
Omid Zahed <omidzahed8@gmail.com>
2024-03-15 22:22:15 +0330

    Refactor: log package (#20)
    * feat: added replace attr function
    
    ---------
    
    Co-authored-by: OZahed <oz@ozaki.com>

---
+++ log/slog.go
@@ -0,0 +1,125 @@
+package log
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+)
+
+// HandlerType determines which type of Handler should be used for the logger
+type HandlerType int
+
+const (
+	JsonHandler HandlerType = 1 + iota
+	TextHandler
+)
+
+// slogOptions is a configuration struct for the ReplaceAttr function
+type slogOptions struct {
+	// HandlerType is the type of handler to be used for the logger
+	HandlerType     HandlerType
+	ReplaceAttrFunc func(groups []string, a slog.Attr) slog.Attr
+	// Level is the level of logging to be used for the logger
+	// Possible levels are "info", "warn", "warning", "error", "err", "debug"
+	Level string
+
+	// ReplaceAttrEnable is a flag to determine if the ReplaceAttr function should be enabled
+	ReplaceAttrEnable bool
+	// AlwaysUTC is a flag to determine if the time should always be in UTC regardless of your system timezone
+	AlwaysUTC bool
+}
+
+type slogOptionFunc func(*slogOptions)
+
+func WithLevel(level string) slogOptionFunc {
+	return func(cfg *slogOptions) {
+		cfg.Level = level
+	}
+}
+
+func WithHandlerType(handlerType HandlerType) slogOptionFunc {
+	return func(cfg *slogOptions) {
+		cfg.HandlerType = handlerType
+	}
+}
+
+func WithAlwaysUTC(enable bool) slogOptionFunc {
+	return func(cfg *slogOptions) {
+		cfg.ReplaceAttrEnable = true
+		cfg.AlwaysUTC = enable
+	}
+}
+
+func WithReplceAttrFunc(replaceAttr func(groups []string, a slog.Attr) slog.Attr) slogOptionFunc {
+	return func(cfg *slogOptions) {
+		cfg.ReplaceAttrEnable = true
+		cfg.ReplaceAttrFunc = replaceAttr
+
+	}
+}
+
+// NewSlog function provides a new logger instance from the slog package
+// with the provided options.
+func NewSlog(opts ...slogOptionFunc) *slog.Logger {
+	// Default Options
+	opt := slogOptions{
+		HandlerType:       TextHandler,
+		Level:             "debug",
+		ReplaceAttrEnable: false,
+	}
+
+	for _, o := range opts {
+		o(&opt)
+	}
+
+	var handlerFunc slog.Handler
+	handlerOptions := &slog.HandlerOptions{
+		AddSource:   true,
+		Level:       getLoggerLevel(opt.Level),
+		ReplaceAttr: makeReplaceAttr(opt),
+	}
+	switch opt.HandlerType {
+	case JsonHandler:
+		handlerFunc = slog.NewJSONHandler(os.Stdout, handlerOptions)
+	default:
+		handlerFunc = slog.NewTextHandler(os.Stdout, handlerOptions)
+	}
+
+	return slog.New(handlerFunc)
+}
+
+func getLoggerLevel(level string) slog.Level {
+	switch strings.ToLower(level) {
+	case "info", "information":
+		return slog.LevelInfo
+	case "warn", "warning":
+		return slog.LevelWarn
+	case "error", "err":
+		return slog.LevelError
+	default:
+		return slog.LevelDebug
+	}
+}
+
+func makeReplaceAttr(cfg slogOptions) func(groups []string, a slog.Attr) slog.Attr {
+	if !cfg.ReplaceAttrEnable {
+		return nil
+	}
+
+	if cfg.ReplaceAttrFunc != nil {
+		return cfg.ReplaceAttrFunc
+	}
+
+	return func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.TimeKey && cfg.AlwaysUTC {
+			a.Value = slog.TimeValue(a.Value.Time().UTC())
+		}
+
+		// On slog Text handler duration is already in text format
+		if cfg.HandlerType == JsonHandler && a.Value.Kind() == slog.KindDuration {
+			a.Value = slog.StringValue(a.Value.Duration().String())
+		}
+
+		return a
+	}
+}
