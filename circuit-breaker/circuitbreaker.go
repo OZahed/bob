e04934a1656db43b9daf24c6113ecd2c23e9c0e4
Omid Zahed <omidzahed8@gmail.com>
2024-04-01 22:16:19 +0330

    Feat: circuit breaker (#30)
    feat: circuit breaker added

--- circuit-breaker/circuitbreaker.go
+++ circuit-breaker/circuitbreaker.go
@@ -1,8 +1,33 @@
+/*
+Package circuitbreaker provides a simple circuit breaker implementation.
+
+State transfer should be statistically evaluated to avoid false positives and negatives.
+*/
+
 package circuitbreaker
 
 import (
+	"math"
+	"net/http"
 	"sync"
 	"time"
+)
+
+const (
+	floatOne = 1.0
+)
+
+var (
+	DefaultHalfOpenPercentages               = []float64{0.1, 0.3, 0.5, 0.75, 1.0}
+	DefaultInterMediatoryStateChangeInterval = time.Second * 1
+)
+
+type State int
+
+const (
+	Closed State = iota
+	Open
+	HalfOpen
 )
 
 type Bucket struct {
@@ -15,33 +40,83 @@ type RetryPolicy struct {
 	Wait  time.Duration
 }
 
-type State int
-
-const (
-	Closed State = iota
-	Open
-	HalfOpen
-)
-
-type StateHandler interface {
-	Allow(currentRate, thr float64) bool
-	StateEval(currentState State)
+type halfOpenInfo struct {
+	LastHalfOpenRequest            time.Time
+	HalfOpenStages                 []float64
+	HalfOpenSubStateChangeInterval time.Duration
+	CurrentPercentage              float64
+	OnFlightRequest                float64
+	MaxRequest                     float64
 }
 
-// TODO: struct padding could be better to reduce the foot print by almost half
+func (h *halfOpenInfo) NextStep() float64 {
+	for idx, percent := range DefaultHalfOpenPercentages {
+		if h.CurrentPercentage == percent {
+			if idx == (len(h.HalfOpenStages) - 1) {
+				return floatOne
+			}
+
+			return h.HalfOpenStages[idx+1]
+		}
+	}
+
+	return 0
+}
+
+func (h *halfOpenInfo) ZeroState() {
+	if h.HalfOpenSubStateChangeInterval <= 0 {
+		h.HalfOpenSubStateChangeInterval = DefaultInterMediatoryStateChangeInterval
+	}
+
+	if len(h.HalfOpenStages) == 0 {
+		h.HalfOpenStages = DefaultHalfOpenPercentages
+	}
+
+	h.LastHalfOpenRequest = time.Time{}
+	h.OnFlightRequest = 0
+	h.CurrentPercentage = h.HalfOpenStages[0]
+}
+
+// HttpRequester is the interface abstracting http.Client
+// if you want to use monitoring and tracing, just implement them on http.Client and provide it to the breaker
+type HttpRequester interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
 type CircuitBreaker struct {
-	mu                     sync.RWMutex
-	lastBucketTime         time.Time
-	handler                StateHandler
-	buckets                []Bucket
-	changeBucketDuration   time.Duration
-	threshold, currentRate float64
-	lastIndex,
-	windowInSeconds,
-	bucketPerSecond,
-	totalRequests,
-	totalFailures int
-	currentState State
+	lastStateChange      time.Time
+	lastBucketTime       time.Time
+	requester            HttpRequester
+	halfOpenInfo         *halfOpenInfo
+	buckets              []Bucket
+	threshold            float64
+	currentRate          float64
+	stateStepInterval    time.Duration
+	changeBucketDuration time.Duration
+	windowInSeconds      int
+	bucketPerSecond      int
+	totalRequests        int
+	totalFailures        int
+	lastIndex            int
+	currentState         State
+	mu                   sync.RWMutex
+}
+
+func (cb *CircuitBreaker) ZeroState() {
+	cb.mu.Lock()
+	defer cb.mu.Unlock()
+
+	cb.lastBucketTime = time.Time{}
+	cb.halfOpenInfo.ZeroState()
+	for idx := range cb.buckets {
+		cb.buckets[idx] = Bucket{}
+	}
+
+	cb.lastIndex = 0
+	cb.currentRate = 1.0
+
+	cb.totalFailures = 0
+	cb.totalRequests = 0
 }
 
 // NewCircuitBreaker creates a new CircuitBreaker with the given windowInSeconds, bucketPerSecond and breakigThreshold.
@@ -49,13 +124,15 @@ type CircuitBreaker struct {
 // The bucketPerSecond is the number of buckets that the windowInSeconds will be divided into.
 // The breakigThreshold is the percentage of failures that will cause the CircuitBreaker to open.
 // The StateHandler is the handler that will be used to evaluate the state of the CircuitBreaker.
-func NewCircuitBreaker(windowInSeconds, bucketsPerSecond int, threshold float64, sh StateHandler) *CircuitBreaker {
+func NewCircuitBreaker(windowInSeconds, bucketsPerSecond int,
+	threshold float64, stateStepInterval time.Duration, req HttpRequester) *CircuitBreaker {
 	return &CircuitBreaker{
-		windowInSeconds: windowInSeconds,
-		bucketPerSecond: bucketsPerSecond,
-		threshold:       threshold,
-		buckets:         make([]Bucket, windowInSeconds*bucketsPerSecond),
-		handler:         sh,
+		windowInSeconds:   windowInSeconds,
+		bucketPerSecond:   bucketsPerSecond,
+		threshold:         threshold,
+		stateStepInterval: stateStepInterval,
+		buckets:           make([]Bucket, windowInSeconds*bucketsPerSecond),
+		requester:         req,
 	}
 }
 
@@ -101,7 +178,7 @@ func (cb *CircuitBreaker) getBucketIndex() int {
 //
 //
 //		// check the status code and return an error if it is not 200
-//		if !(res.StatusCode > 100 && res.StatusCode < 400){
+//		if !(res.StatusCode >= 200 && res.StatusCode < 400){
 //			return errors.New("server returned non-200 status code")
 //		}
 //
@@ -115,13 +192,13 @@ func (cb *CircuitBreaker) getBucketIndex() int {
 //		}
 //		return nil
 //	})
-func (cb *CircuitBreaker) MakeRequest(f func() error) error {
+func (cb *CircuitBreaker) Execute(f func() error) error {
 	cb.mu.Lock()
 	defer cb.mu.Unlock()
 
-	defer cb.handler.StateEval(cb.currentState)
+	defer cb.StateEval()
 
-	if !cb.handler.Allow(cb.currentRate, cb.threshold) {
+	if !cb.Allow() {
 		return ErrRequestDropped
 	}
 
@@ -137,9 +214,17 @@ func (cb *CircuitBreaker) MakeRequest(f func() error) error {
 	}
 
 	cb.updateStats()
-	cb.handler.StateEval(cb.currentState)
+	cb.StateEval()
 
 	return err
+}
+
+func (cb *CircuitBreaker) DoRequest(req *http.Request) (resp *http.Response, err error) {
+	err = cb.Execute(func() error {
+		resp, err = cb.requester.Do(req)
+		return err
+	})
+	return
 }
 
 func (cb *CircuitBreaker) updateStats() {
@@ -150,5 +235,72 @@ func (cb *CircuitBreaker) Allow() bool {
 	cb.mu.RLock()
 	defer cb.mu.RUnlock()
 
-	return cb.handler.Allow(cb.currentRate, cb.threshold)
+	switch cb.currentState {
+	case Closed:
+		return cb.closedAllow()
+	case Open:
+		return false
+	case HalfOpen:
+		return cb.halfOpenAllow()
+	default:
+		return false
+	}
+}
+
+func (cb *CircuitBreaker) closedAllow() bool {
+	return cb.currentRate < cb.threshold
+}
+
+// TODO: check for todo section
+// checko for HalfOpen allow
+func (cb *CircuitBreaker) halfOpenAllow() bool {
+	if time.Since(cb.halfOpenInfo.LastHalfOpenRequest) > cb.halfOpenInfo.HalfOpenSubStateChangeInterval {
+		cb.checkHalfOpenState()
+	}
+
+	allowedReqNumbers := cb.halfOpenInfo.MaxRequest * cb.halfOpenInfo.CurrentPercentage
+
+	if allowedReqNumbers < 1.0 {
+		allowedReqNumbers = 1.0
+	}
+
+	return math.Abs(allowedReqNumbers-cb.halfOpenInfo.OnFlightRequest) < 0.01
+
+}
+
+func (cb *CircuitBreaker) checkHalfOpenState() {
+	if (time.Since(cb.halfOpenInfo.LastHalfOpenRequest) >= cb.halfOpenInfo.HalfOpenSubStateChangeInterval) &&
+		float64(cb.totalFailures)/float64(cb.totalRequests) > 0.9 {
+		cb.halfOpenInfo.CurrentPercentage = cb.halfOpenInfo.NextStep()
+	}
+
+	if cb.halfOpenInfo.CurrentPercentage == 0 {
+		return
+	}
+
+	if cb.halfOpenInfo.CurrentPercentage > 0.9 {
+		cb.setState(Closed)
+	}
+}
+
+func (cb *CircuitBreaker) setState(state State) {
+	cb.ZeroState()
+	cb.lastStateChange = time.Now()
+	cb.currentState = state
+}
+
+// Bring everuything here
+func (cb *CircuitBreaker) StateEval() {
+	switch cb.currentState {
+	case HalfOpen:
+		cb.checkHalfOpenState()
+	case Open:
+		if time.Since(cb.lastStateChange) > cb.stateStepInterval {
+			cb.setState(HalfOpen)
+		}
+	case Closed:
+		if cb.currentRate < cb.threshold {
+			cb.setState(Closed)
+		}
+	}
 }
